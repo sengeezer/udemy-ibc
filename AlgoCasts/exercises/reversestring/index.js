7ec36cf552151b10db8a22d41e447a49cd9a82e8
Fabio Albertin <albertin@gmail.com>
2017-11-22 23:01:10 +0000

    Completed exercise 1

--- AlgoCasts/exercises/reversestring/index.js
+++ AlgoCasts/exercises/reversestring/index.js
@@ -6,6 +6,16 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let newstr = '';
+  for (let idx = str.length - 1; idx >= 0; idx--) {
+    newstr += str[idx];
+  }
+
+  return newstr;
+}
+
+// could have done
+// return [...str].reverse().join('');
 
 module.exports = reverse;
