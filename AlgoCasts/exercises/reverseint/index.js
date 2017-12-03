456fb460ba23f343ec78608f920893d769214d4a
Fabio Albertin <albertin@gmail.com>
2017-12-03 23:47:28 +0000

    Completed reverseInt

--- AlgoCasts/exercises/reverseint/index.js
+++ AlgoCasts/exercises/reverseint/index.js
@@ -8,6 +8,18 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let reversed = [...n.toString()].reverse();
+
+  if (Math.sign(n) === -1) {
+    const removed = reversed.pop();
+    reversed = [removed, reversed.join('')];
+  }
+
+  return parseInt(reversed.join(''), 10);
+}
+
+// could also move - with x * -1
+// parseInt(reversed) * Math.sign(n);
 
 module.exports = reverseInt;
